4e6d18d76ed67c82965461669bb92a59d8306c82
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-03-22 18:10:27 +0100

    text variable

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ This module starts a Flask web application """
 
-from flask import Flask, escape
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -24,8 +24,9 @@ def display_c_text(text):
     Display "C" followed by the value of the text variable.
     Replaces underscore _ symbols with a space.
     """
-    return 'C {}'.format(escape(text).replace('_', ' '))
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
+    """ Run the app on port 5000 """
     app.run(host='0.0.0.0', port=5000)
