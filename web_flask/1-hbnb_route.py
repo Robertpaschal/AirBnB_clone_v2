f97357b2ee511acd8e452c812110ba8c15f6479b
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-03-22 17:43:09 +0100

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,18 +5,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 def hbnb_hello():
     """ Display "Hello HBNB!" when visiting the route URL """
     return 'Hello HBNB!'
+
 
 def hbnb():
     """ Displays "HBNB" when visiting the /hbnb URL """
     return 'HBNB'
 
+
 @app.route('/', strict_slashes=False)
 def hbnb_hello_route():
     """ Route to display Hello HBNB! """
     return hbnb_hello()
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
@@ -24,5 +28,5 @@ def hbnb_route():
     return hbnb()
 
 
-if __name__== '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
