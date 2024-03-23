f9290c7fff3067e7a3b50cf8cb38ef59b4ed3d54
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-03-23 04:11:00 +0100

    odd or even rendering

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -52,7 +52,10 @@ def display_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def display_odd_or_even(n):
     """ display a HTML page only if n is an integer"""
-    return render_template('6-number_odd_or_even.html', n=n, odd_even='even' if n % 2 == 0 else 'odd')
+    return render_template(
+        '6-number_odd_or_even.html', n=n,
+        odd_even='even' if n % 2 == 0 else 'odd')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
